c96f8a1cdb331a62bdfc1dde45011525e961fb14
aayushsin <aayushsingla.bits@gmail.com>
2020-01-07 21:05:46 +0100

    Minor

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: `/gatsby-creative`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `3XCEED`,
+    description: `Business website for 3xceed.com`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default class IndexPage extends React.Component {
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-8 text-center">
-                <h2 className="text-white mt-0">We've got what you need!</h2>
+                <h2 className="text-white mt-0">Really!</h2>
                 <hr className="divider light my-4"/>
                 <p className="text-white-50 mb-4">Start Bootstrap has everything you need to get your new website up
                   and
